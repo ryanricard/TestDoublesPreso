186c14eba38657e59b34fbcc8b15acf6ded68adb
Ryan Ricard <ryan.ricard@gmail.com>
2017-05-03 19:46:53 -0500

    Add nondeterministic test

---
+++ test/nondeterministic/RollTest.js
@@ -0,0 +1,28 @@
+const expect = require('chai').expect;
+const Roll = require('../../lib/Roll');
+
+describe('Deterministic Roll', () => {
+  it('should default to two dice', () => {
+    const roll = new Roll();
+    expect(roll.dice.length).to.equal(2);
+  });
+
+  it('should provide the ability to roll more than two dice', () => {
+    const roll = new Roll(5);
+
+    expect(roll.dice.length).to.equal(5);
+  });
+
+  it('should handle invalid numeric input', () => {
+    const roll = new Roll(-2);
+
+    expect(roll.dice.length).to.equal(0);
+    expect(roll.total).to.equal(0);
+  });
+
+  it('should correctly compute dice total', () => {
+    const roll = new Roll();
+
+    expect(roll.total).to.equal(10);
+  });
+});
